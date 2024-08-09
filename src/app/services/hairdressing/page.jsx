b88d10a6733ca0e989ff894087af00e2d8f005cf
Katherina Fanina <katherina.fanina@gmail.com>
2024-08-09 12:13:38 +0300

    fill mobile haircut  page

--- src/app/services/hairdressing/page.jsx
+++ src/app/services/hairdressing/page.jsx
@@ -10,6 +10,13 @@ import IconManHaircutting from '@components/icons/IconManHair';
 import IconHair from '@components/icons/IconHair';
 import shapeFlower from '@public/flower_shape_4.png';
 import dyed_hair from '@public/dyed_hair.jpg';
+import dyed_hairs from '@public/dyed_hair_2.jpg';
+import menHaircut from '@public/men_haircut.png';
+import kidsHaircut from '@public/kids_haircut.png';
+import shortAndLongtHaircut from '@public/short__long_haircut.png';
+import creativeHaircut from '@public/creative_haircut.png';
+import classicHaircut from '@public/classic_haircut.png';
+import trandyHaircut from '@public/trandy_haircut.png';
 import styles from './hairdressing.module.css';
 
 
@@ -22,31 +29,38 @@ export const metadata = {
 export default function HairdressingPage() {
   const haircutDatas = [
     {
+      getImg: () => <Image src={menHaircut} alt='man-haircut' />,
       id: 'men-haircut',
       getIcon: () => <IconManHaircutting size={40} />,
       lable: 'Стильні та зручні чоловічі',
     },
     {
+      // getImg: () => <Image src={womanHaircut} alt='woman-haircut' />,
+      getImg: () => <Image src={trandyHaircut} alt='woman-haircut' />,
       id: 'women-haircut',
       getIcon: () => <IconWomenHaircut size={40} />,
       lable: 'Модні та оригінальні жіночі',
     },
     {
+      getImg: () => <Image src={kidsHaircut} alt='girl-haircut' />,
       id: 'children-haircut',
       getIcon: () => <IconChild size={40} />,
       lable: 'Професійні дитячі',
     },
     {
+      getImg: () => <Image src={classicHaircut} alt='classic-haircut' />,
       id: 'classic-haircut',
       getIcon: () => <IconLongHair size={40} />,
       lable: 'Класичні та креативні',
     },
     {
+      getImg: () => <Image src={shortAndLongtHaircut} alt='long-hair-haircut' />,
       id: 'short-haircut',
       getIcon: () => <IconBackHair size={40} />,
       lable: 'На коротке та довге волосся',
     },
     {
+      getImg: () => <Image src={creativeHaircut} alt='short-haircut' />,
       id: 'asymmetrical-haircut',
       getIcon: () => <IconShortHair size={40} />,
       lable: 'Асиметричні та нестандартні',
@@ -112,8 +126,14 @@ export default function HairdressingPage() {
               <ul>
                 {haircutDatas.map((haircutData) => (
                   <li className={styles.haircutItem} key={haircutData.id}>
-                    <div className={styles.iconWrapper}>{haircutData.getIcon()}</div>
+                    <div className={styles.iconWrapper}>{haircutData.getIcon()}
+                    </div>
+                    <div className={styles.hairSubTitle} >
                     <p className={styles.hairText}>{haircutData.lable} </p>
+                    </div>
+                    <div className={styles.imageWrapper}>
+                  {haircutData.getImg()}
+                </div>
                   </li>
                 ))}
               </ul>
@@ -132,6 +152,9 @@ export default function HairdressingPage() {
                   </div>
                   <h2 className={styles.haircutTitle}>Фарбування волосся</h2>
                 </div>
+                <div className={styles.hairImg}>
+                <Image src={dyed_hairs} alt='hair-dyeing' />
+              </div>
                 <ul>
                   {hairDatas.map((hairData) => (
                     <li className={styles.hairItem} key={hairData.id}>
